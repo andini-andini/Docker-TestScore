de4dee3870f7c02797d3dc929c6f1201fc6d6eb5
andini-andini <aandini072@gmail.com>
2021-11-30 11:46:23 +0700

    Update file

--- testScore.java
+++ testScore.java
@@ -1,31 +1,80 @@
 import java.util.Scanner;
 
-public class TestScore {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        
-        int score;
-        
-        System.out.println("Enter test score (0 - 100) : ");
-        score = sc.nextInt();
-        if(score >= 0 && score <= 100){
-             if (score >= 90 && score <= 100){
-                 System.out.println("Score A, EXCLLENT!");
-             } else if (score >= 80 && score <= 89){
-                System.out.println("Score B, keep your achievements!");
-             } else if (score >= 60 && score <= 79){
-                 System.out.println("Score C, keep your achievements!");
-             } else if (score >= 50 && score <= 59){
-                 System.out.println("Score D, keep your achievements!");
-             } else {
-                 System.out.println("Score E, do not pass!");
-             }
-        } else {
-            if(score <0 && score> 100){
-                System.out.println("The value you entered less than 0");
-            }else{
-                System.out.println("The value you entered more than 100");
+public class Matriks_kali {
+    public  static  void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Baris Matriks A");
+        int b = input.nextInt();
+
+        System.out.println("Masukkan Kolom Matriks A");
+        int k = input.nextInt();
+
+        int[][] A = new int[b][k];
+
+        for (int a = 0; a < A.length; a++){
+            for (int c = 0; c < A[a].length; c++){
+                System.out.print("Nilai Matriks A[" + a + "]" + "[" + c + "] : ");
+                A[a][c] = input.nextInt();
             }
         }
+
+        for (int a = 0; a < A.length; a++){
+            for (int c = 0; c < A[a].length; c++){
+                System.out.print(A[a][c] + "\t");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Masukkan Baris Matriks B");
+        int b2 = input.nextInt();
+
+        System.out.println("Masukkan Kolom Matriks b");
+        int k2 = input.nextInt();
+
+
+        if (k != b2){
+            System.out.println("Maaf, kolom matriks A dan baris Matriks B tidak sama");
+        }else {
+
+            int sum =0;
+            int[][] B = new int[b2][k2];
+
+            for (int a = 0; a < B.length; a++) {
+                for (int c = 0; c < B[a].length; c++) {
+                    System.out.print("Nilai Matriks B[" + a + "]" + "[" + c + "] : ");
+                    B[a][c] = input.nextInt();
+                }
+            }
+
+            for (int a = 0; a < B.length; a++) {
+                for (int c = 0; c < B[a].length; c++) {
+                    System.out.print(B[a][c] + "\t");
+                }
+                System.out.println("");
+            }
+
+            int[][] C = new int[b][k2];
+
+            for (int i = 0; i < C.length; i++){
+                for (int j = 0; j < C[i].length ; j++){
+                    for (int K = 0; K < b2;K++){
+                        sum += A[i][K] * B[K][j];
+                    }
+                    C[i][j] = sum;
+                    sum = 0;
+                }
+            }
+
+            System.out.println("Hasil Perkalian Matriks A dan B");
+            for (int i = 0; i < C.length; i++){
+                for (int j = 0; j < C[i].length ; j++){
+                   System.out.print(C[i][j] + "\t");
+                }
+                System.out.println("");
+            }
+
+        }
+
     }
 }
